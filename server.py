051ae7bf64fcf8811e0db5d3d4f819d6bc2c05f6
BuildTools <unconfigured@null.spigotmc.org>
2023-02-21 18:43:16 -0500

    e

--- server.py
+++ server.py
@@ -1,21 +1,16 @@
-from flask import Flask, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Define the total_supply variable here
-total_supply = 1000000
+# define your routes here
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route('/total_supply', methods=['GET'])
-def get_total_supply():
-    global total_supply
-    return str(total_supply)
-
-@app.route('/mine', methods=['POST'])
-def mine():
-    global total_supply
-    mined_coins = int(request.form['coins'])
-    total_supply += mined_coins
-    return "Mined {} coins. Total supply is now {}.".format(mined_coins, total_supply)
+@app.route('/total_supply')
+def total_supply():
+    return str(1000000)
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=8080)
+    app.run(debug=True, port=5000) # run on port 5000 for HTML requests
+    app.run(debug=True, port=8080) # run on port 8080 for total supply requests
