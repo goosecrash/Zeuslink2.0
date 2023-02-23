7e1d7d3ac17ef2ac3907c5510bac1bec7d82c771
BuildTools <unconfigured@null.spigotmc.org>
2023-02-23 10:21:22 -0500

    hello

--- htmlserver.py
+++ htmlserver.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+import requests
 
-app = Flask(__name__)
+url = 'http://localhost:5000/total_supply'
+def get_total_supply():
+    try:
+        response = requests.get('http://localhost:8080/total_supply')
+        response.raise_for_status()  # Raise an exception if the response status code is not 200
+        return int(response.text)
+    except requests.exceptions.RequestException as e:
+        print('Error:', e)
+        return None
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
+try:
+    response = requests.get(url, timeout=1)
+    if response.status_code == 200:
+        print('Total supply:', response.text)
+    else:
+        print('Error: Could not get total supply')
+except requests.exceptions.RequestException as e:
+    print('Error:', e)
 if __name__ == '__main__':
-    app.run(debug=True)
+    total_supply = get_total_supply()
+    if total_supply is not None:
+        print(f'Total supply: {total_supply}')
--- server.py
+++ server.py
@@ -1,16 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, request
 
 app = Flask(__name__)
 
-# define your routes here
-@app.route('/')
-def index():
-    return render_template('index.html')
+# Define the total_supply variable here
+total_supply = 1000000
 
-@app.route('/total_supply')
-def total_supply():
-    return str(1000000)
+@app.route('/total_supply', methods=['GET'])
+def get_total_supply():
+    global total_supply
+    return str(total_supply)
+
+@app.route('/mine', methods=['POST'])
+def mine():
+    global total_supply
+    mined_coins = int(request.form['coins'])
+    total_supply += mined_coins
+    return "Mined {} coins. Total supply is now {}.".format(mined_coins, total_supply)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000) # run on port 5000 for HTML requests
-    app.run(debug=True, port=8080) # run on port 8080 for total supply requests
+    app.run(host='localhost', port=8080)
